6c76c27a005ea7ebb9840456f59e8ac3b68f291d
Nikoloz Chapichadze <nikoloz.chapichadze@gmail.com>
2020-10-16 01:14:12 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,7 +2,7 @@ def find_max_value(array)
   counter = 0
   max = array[0]
   while array[counter] do 
-    if(max < array[counter]) do
+    if(max < array[counter]) 
       max = array[counter]
     end
     counter+=1 
